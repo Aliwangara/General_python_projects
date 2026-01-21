f9db04d7a736e9485ee86626d24671a72ae535db
Ali Wangara <Aliwangara63@gmail.com>
2026-01-21 22:49:42 +0300

    worked with file hndling in python

--- 2026_exercises/19-01-2026/file_handling.py
+++ 2026_exercises/19-01-2026/file_handling.py
@@ -3,7 +3,17 @@
 def save_students(student_name,average_marks):
 
     with open("2026_exercises/19-01-2026/students.txt",'a',newline="") as f:
-        writer = f.write(f"{student_name} - {average_marks}")
+        
+        average_marks = sum(average_marks)/len(average_marks) 
+        writer = f.write(f"{student_name} - {average_marks}\n")
 
-save_students("wangara", 70) 
 
+def read_students():
+    with open("2026_exercises/19-01-2026/students.txt", 'r') as f:
+        for line in f.readlines():
+            print(line.strip())
+
+
+save_students("Ali", [20,30,40,50]) 
+
+read_students()
